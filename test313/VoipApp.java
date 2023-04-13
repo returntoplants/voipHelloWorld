e0d442007229bcbb1adac9f9441f2d0e20f295d5
athini <24695424@sun.ac.za>
2023-04-13 23:22:34 +0200

    application app threading part 2

--- test313/VoipApp.java
+++ test313/VoipApp.java
@@ -2,20 +2,60 @@ import java.net.*;
 import java.io.*;
 
 public class VoipApp {
+    public static String RECEIVER = "receiver";
+    public static String SENDER   = "sender";
     public VoipSender sender;
     public VoipReceiver receiver;
+    public VoipRunner runner;
 
-    public VoipApp(VoipSender sender,VoipReceiver receiver,String address,int port) {
-        this.sender = new VoipSender(address,port);
+    public VoipApp(VoipSender sender,VoipReceiver receiver,String address,int port,String role) {
         this.receiver = new VoipReceiver(port);
+        this.sender = new VoipSender(address,port);
         
-        this.sender.start();
-        this.receiver.start();
+        runner = new VoipRunner(sender,receiver,role);
+        runner.start();
+    }
+    private class VoipRunnner implements Runnable {
+        private Thread t;
+        private String role;
+        VoipSender sender;
+        VoipReceiver receiver;
+
+        public VoipRunnner(VoipSender sender,VoipReceiver receiver,String role) {
+            this.sender = sender;
+            this.receiver = receiver;
+            this.role = role;
+        }
+        
+        public void run() {
+            switch(this.role) {
+                case "receiver":
+                    this.receiver.start();
+                    while (True) {
+                        //keep on trying to send.
+                        this.sender.start();
+                    }
+                    break;
+                case "sender":
+                    this.sender.start();
+                    this.receiver.start();
+                    break;
+            }
+        }
+
+        public void start() {
+            if (t == null) {
+                t = new Thread(this,this.role);
+                t.start();
+            }
+        }
     }
 
     public static void main(String[] args) {
         String address = args[0];
         int port       = Integer.parseInt(args[1]);
+        String role    = args[2];
+
         VoipSender sender = new VoipSender(address, port);
         VoipReceiver receiver = new VoipReceiver(port);
         VoipApp app = new  VoipApp(sender, receiver, address, port);       
