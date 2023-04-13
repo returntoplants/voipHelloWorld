0c9098bfa7c4f74b3b9712b57f07ded53693967e
athini <24695424@sun.ac.za>
2023-04-13 22:28:26 +0200

    application with threading

--- test313/VoipApp.java
+++ test313/VoipApp.java
@@ -4,7 +4,20 @@ import java.io.*;
 public class VoipApp {
     public VoipSender sender;
     public VoipReceiver receiver;
-    public static void main(String[] args) {
 
+    public VoipApp(VoipSender sender,VoipReceiver receiver,String addresss,int port) {
+        this.sender = new VoipSender(receiverAddress,port);
+        this.receiver = new VoipReceiver(port);
+        
+        this.sender.start();
+        this.receiver.start();
+    }
+
+    public static void main(String[] args) {
+        String address = args[0];
+        int port       = Integer.parseInt(args[1]);
+        VoipSender sender = new VoipSender(address, port);
+        VoipReceiver receiver = new VoipReceiver(port);
+        VoipApp app = new  VoipApp(sender, receiver, addresss, port);       
     }
 }
