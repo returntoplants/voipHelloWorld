4af5aacdeb2cd33b7c01ecc180c4176c979da485
athini <24695424@sun.ac.za>
2023-04-13 18:06:43 +0200

    working voip tes

--- test313/VoipReceiver.java
+++ test313/VoipReceiver.java
@@ -1,7 +1,9 @@
 import java.net.*;
 
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.AudioFormat;
 
 import java.io.*;
 
@@ -10,27 +12,39 @@ public class VoipReceiver {
     private AudioFormat format;
 
     public VoipReceiver(int port) {
-        format = new AudioFormat(8000.0f,16,1,true,true);
+        try {
+            format = new AudioFormat(8000.0f,16,1,true,true);
 
-        socket = new DatagramSocket(port); 
+            socket = new DatagramSocket(port); 
 
-        this.receive();
+            this.receive();
+        }
+        catch(IOException io) {
+            System.out.println(io);
+            io.printStackTrace();
+        }
     }
 
-    public void receive() {
-        SourceDataLine speakers = AudioSystem.getSourceDataLine(format);
+    public void receive() throws IOException {
+        try {
+            SourceDataLine speakers = AudioSystem.getSourceDataLine(format);
 
-        speakers.open(format);
-        speakers.start();
+            speakers.open(format);
+            speakers.start();
 
-        byte[] buffer = new byte[1024];
+            byte[] buffer = new byte[1024];
 
-        // Continuously receive audio data over UDP and play it back on the
-        // speakers.
-        while (true) {
-            DatagramPacket packet = new DatagramPacket(buffer,buffer.length);
-            socket.receive(packet);
-            speakers.write(packet.getData(),0,packet.getLength());
+            // Continuously receive audio data over UDP and play it back on the
+            // speakers.
+            while (true) {
+                DatagramPacket packet = new DatagramPacket(buffer,buffer.length);
+                socket.receive(packet);
+                speakers.write(packet.getData(),0,packet.getLength());
+            }
+        }
+        catch (LineUnavailableException lu) {
+            System.out.println(lu);
+            lu.printStackTrace();
         }
     }
 
--- test313/VoipSender.java
+++ test313/VoipSender.java
@@ -2,6 +2,7 @@ import java.net.*;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 
 import java.io.*;
@@ -28,37 +29,49 @@ public class VoipSender {
 
         }
         catch (IOException io) {
-            sysSystem.out.println(io);
+            System.out.println(io);
             io.printStackTrace();
         }
     }
 
     public void call() throws IOException {
-        // the microphone input stream.
-        TargetDataLine microphone = AudioSystem.getTargetDataLine(format);
+        try {
+            // the microphone input stream.
+            TargetDataLine microphone = AudioSystem.getTargetDataLine(format);
 
-        //the microphone.
-        microphone.open(format);
-        microphone.start();
+            //the microphone.
+            microphone.open(format);
+            microphone.start();
 
-        //create a buffer to hold the audio data.
-        byte[] buffer = new byte[1024];
+            //create a buffer to hold the audio data.
+            byte[] buffer = new byte[1024];
 
-        // Continuouslty read audio data from the microphone and send it over UDP
-        while (true) {
-            int count = microphone.read(buffer,0,buffer.length);
+            // Continuouslty read audio data from the microphone and send it over UDP
+            while (true) {
+                int count = microphone.read(buffer,0,buffer.length);
 
-            // the datagram packet.
-            Datagram packet = new DatagramPacket(buffer,count,dest,destPort);
+                // the datagram packet.
+                DatagramPacket packet = new DatagramPacket(buffer,count,dest,destPort);
 
-            socket.send(packet);   // send the data packet over the socket.
+                socket.send(packet);   // send the data packet over the socket.
+            }
         }
-
+        catch (LineUnavailableException lu) {
+            System.out.println(lu);
+            lu.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         String receiverAddr = args[0];
         int port = Integer.parseInt(args[1]);
-        VoipSender send = new VoipSender(receiverAddress, port); 
+        VoipSender send = new VoipSender(receiverAddr, port);
+        try {
+            send.call();    // start the call.
+        }
+        catch(IOException io) {
+            System.out.println(io);
+            io.printStackTrace();
+        }
     }
 }
