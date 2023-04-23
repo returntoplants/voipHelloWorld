752176d5207f1abbc23cf9f3ad2c1f38c0a85846
athini <24695424@sun.ac.za>
2023-04-23 19:50:52 +0200

    maybe working conference calls

--- test313/VoipApp.java
+++ test313/VoipApp.java
@@ -12,8 +12,8 @@ public class VoipApp {
     public VoipReceiver receiver;
     public VoipRunner runner;
 
-    public VoipApp(String receiver,String myAddress,int port,String role,String call) {
-        this.receiver = new VoipReceiver(port,call,myAddress);
+    public VoipApp(String receiver,String myAddress,int port,String role,String call,String netIf) {
+        this.receiver = new VoipReceiver(port,call,myAddress,netIf);
         if (call.equals("group")) {
             this.sender = new VoipSender("228.0.0.0",myAddress,port,call);
         }
@@ -68,6 +68,10 @@ public class VoipApp {
         int port       = Integer.parseInt(args[2]);
         String role    = args[3];
         String call    = args[4];
-        VoipApp app = new  VoipApp(receiverAddr,myAddress,port,role,call);       
+        String netIf   = args[5];
+        if (netIf.equals("default")) {
+            netIf = "zt44xfkmyl";
+        }
+        VoipApp app = new  VoipApp(receiverAddr,myAddress,port,role,call,netIf);       
     }
 }
