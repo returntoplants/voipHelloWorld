4f3c643ecfbff0a9ce186155a8b5f907755b878d
athini <24695424@sun.ac.za>
2023-04-24 14:58:28 +0200

    commiting voip application

--- test313/VoipReceiver.java
+++ test313/VoipReceiver.java
@@ -75,7 +75,7 @@ public class VoipReceiver implements Runnable {
             speakers.open(format);
             speakers.start();
 
-            byte[] buffer = new byte[256];
+            byte[] buffer = new byte[128];
 
             // Continuously receive audio data over UDP and play it back on the
             // speakers.
